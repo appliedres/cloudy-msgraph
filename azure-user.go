4a69da44976fb0701978cb94ddf8e09eaf2fa7ca
John Bauer <jbauer@appliedres.com>
2022-05-25 09:45:57 -0400

    INitial

---
+++ azure-user.go
@@ -0,0 +1,257 @@
+package azure
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+
+	"github.com/appliedres/cloudy"
+	cloudymodels "github.com/appliedres/cloudy/models"
+)
+
+const InvalidInstanceName = errors.New("invalid instance name")
+
+const Azure = "azure"
+
+const MSGraphVersionV1 = "v1.0"
+const MSGraphVersionBeta = "beta"
+
+var DefaultUserSelectFields = []string{
+	"businessPhones",
+	"displayName",
+	"givenName",
+	"id",
+	"jobTitle",
+	"mail",
+	"mobilePhone",
+	"officeLocation",
+	"surname",
+	"userPrincipalName",
+	"assignedLicenses",
+	"companyName",
+}
+
+type MSGraphInstance struct {
+	Name  string
+	Login string
+	Base  string
+}
+
+var USGovernment = MSGraphInstance{
+	Name:  "USGovernment",
+	Login: "https://login.microsoftonline.us/",
+	Base:  "https://graph.microsoft.us/",
+}
+
+var AzurePublic = MSGraphInstance{
+	Name:  "Public",
+	Login: "https://login.microsoftonline.com/",
+	Base:  "https://graph.microsoft.com/",
+}
+
+func init() {
+	cloudy.UserProviders.Register("azure", func(cfg interface{}) (cloudy.Users, error) {
+		azum := &AzureUserManager{}
+		err := azum.Configure(cfg)
+		return azum, err
+	})
+}
+
+type AzureUserManager struct {
+	Client  *msgraphsdk.GraphServiceClient
+	Adapter *msgraphsdk.GraphRequestAdapter
+	Cfg     *AzureUserConfig
+}
+
+type AzureUserConfig struct {
+	TenantID     string
+	ClientID     string
+	ClientSecret string
+	Region       string
+	APIBase      string
+	Version      string
+	SelectFields []string
+}
+
+func (azcfg *AzureUserConfig) SetInstanceName(name string) error {
+	if strings.EqualFold(name, USGovernment.Name) {
+		azcfg.SetInstance(USGovernment)
+		return nil
+	} else if strings.EqualFold(name, AzurePublic.Name) {
+		azfcg.SetInstance(AzurePublic)
+		return nil
+	}
+
+	return InvalidInstanceName
+}
+
+func (azcfg *AzureUserConfig) SetInstance(instance *MSGraphInstance) {
+	azcfg.APIBase = instance.Base
+	azcfg.Region = instance.Login
+}
+
+func (azum *AzureUserManager) Configure(cfg interface{}) error {
+	azCfg := cfg.(AzureUserConfig)
+
+	if azCfg.Version == "" {
+		azCfg.Version = MSGraphVersionV1
+	}
+	if azCfg.Region == "" {
+		azCfg.SetInstance(&AzurePublic)
+	}
+	if azCfg.SelectFields == nil {
+		azCfg.SelectFields = DefaultUserSelectFields
+	}
+
+	scopes := []string{"https://graph.microsoft.us/.default"}
+
+	cred, err := azidentity.NewClientSecretCredential(azCfg.TenantID, azCfg.ClientID, azCfg.ClientSecret,
+		&azidentity.ClientSecretCredentialOptions{
+			ClientOptions: policy.ClientOptions{
+				Cloud: cloud.AzureGovernment,
+			},
+		})
+
+	if err != nil {
+		fmt.Printf("Error authentication provider: %v\n", err)
+		return err
+	}
+	auth, err := a.NewAzureIdentityAuthenticationProviderWithScopes(cred, scopes)
+	if err != nil {
+		fmt.Printf("Error authentication provider: %v\n", err)
+		return err
+	}
+	adapter, err := msgraphsdk.NewGraphRequestAdapter(auth)
+	if err != nil {
+		fmt.Printf("Error creating adapter: %v\n", err)
+		return err
+	}
+	adapter.SetBaseUrl("https://graph.microsoft.us/v1.0")
+
+	azum.Cfg = &azCfg
+	azum.Adapter = adapter
+	azum.Client = msgraphsdk.NewGraphServiceClient(adapter)
+}
+
+func cfgFromMap(cfgMap map[string]interface{}) *AzureUserConfig {
+	cfg := &AzureUserConfig{}
+
+	cfg.TenantID, _ = cloudy.MapKeyStr(cfgMap, "TenantID", true)
+	cfg.ClientID, _ = cloudy.MapKeyStr(cfgMap, "ClientID", true)
+	cfg.ClientSecret, _ = cloudy.MapKeyStr(cfgMap, "ClientSecret", true)
+	cfg.Region, _ = cloudy.MapKeyStr(cfgMap, "Region", true)
+	cfg.APIBase, _ = cloudy.MapKeyStr(cfgMap, "APIBase", true)
+
+	return cfg
+}
+
+func (azum *AzureUserManager) DebugSerialize(v serialization.Parsable) {
+	writerRegistry := azum.Adapter.GetSerializationWriterFactory() // Returns SerializationWriterFactoryRegistry
+	myWriter, err := writerRegistry.GetSerializationWriter("application/json")
+	if err != nil {
+		fmt.Printf("Unable to retrieve writer: %v\n", err)
+	}
+	// err2 := body.Serialize(myWriter)
+	err2 := myWriter.WriteObjectValue("", v)
+
+	if err2 != nil {
+		fmt.Printf("Unable to serialize: %v\n", err2)
+	}
+	bodyBytes, _ := myWriter.GetSerializedContent() // ([]byte, error)
+	fmt.Println(string(bodyBytes))
+}
+
+func (azum *AzureUserManager) ToAzure(user *cloudymodels.User) *models.User {
+	u := models.NewUser()
+
+	//TODO : Finish
+	if user.UserName != "" {
+		u.UserName = &user.UserName
+	}
+
+	return u
+}
+
+func (azum *AzureUserManager) ToCloudy(user models.Userable) *cloudymodels.User {
+	u := &cloudymodels.User{}
+
+	if user.GetUserPrincipalName() != nil {
+		u.UserName = *user.GetUserPrincipalName()
+	}
+
+	return u
+}
+
+func (azum *AzureUserManager) NewUser(ctx context.Context, newUser *cloudymodels.User) (*cloudymodels.User, error) {
+	// upn := "TESTFIRST.TESTLAST@skyborg.onmicrosoft.us"
+	// profile := models.NewPasswordProfile()
+	// profile.SetForceChangePasswordNextSignIn(boolP(true))
+	// profile.SetPassword(stringP("abcde1234$%#^ASDF"))
+
+	// body := models.NewUser()
+	// body.SetAccountEnabled(boolP(true))
+	// body.SetUserPrincipalName(&upn)
+	// body.SetDisplayName(stringP("TESTFIRST TESTLAST"))
+	// body.SetGivenName(stringP("TESTFIRST"))
+	// body.SetSurname(stringP("TESTLAST"))
+	// body.SetCompanyName(stringP("TESTCOMPANY"))
+	// body.SetJobTitle(stringP("TESTJOBTITLE"))
+	// body.SetMailNickname(stringP("TESTFIRST.TESTLAST"))
+	// body.SetPasswordProfile(profile)
+	// body.SetBusinessPhones([]string{"+1 937-111-1111"})
+	// body.SetMobilePhone(stringP("+1 937-111-1111"))
+
+	body := azum.ToAzure(newUser)
+
+	user, err := graph.Client.Users().Post(body)
+	if err != nil {
+		return err
+	}
+	return newUser, nil
+}
+
+func (azum *AzureUserManager) GetUser(ctx context.Context, uid string) (*models.User, error) {
+	return nil, nil
+}
+
+func (azum *AzureUserManager) ListUsers(ctx context.Context, page interface{}, filter interface{}) ([]*models.User, interface{}, error) {
+	return nil, nil, nil
+}
+
+func (azum *AzureUserManager) UpdateUser(ctx context.Context, usr *models.User) (bool, error) {
+	return false, nil
+}
+
+func (azum *AzureUserManager) Enable(ctx context.Context, uid string) (bool, error) {
+	return false, nil
+}
+
+func (azum *AzureUserManager) Disable(ctx context.Context, uid string) (bool, error) {
+	return false, nil
+}
+
+func (azum *AzureUserManager) DeleteUser(ctx context.Context, uid string) (bool, error) {
+	return false, nil
+}
+
+/// Other thinsgs that MS Graph can do
+
+func (azum *AzureUserManager) AddRemoveLicenses(ctx context.Context, uid string, skusToAdd []string, skusToRemove []string) error {
+	return false, nil
+}
+
+func (azum *AzureUserManager) SetLicenses(ctx context.Context, uid string, skus []string) error {
+	// Get the user
+
+	licenses := user.Get
+
+	return false, nil
+}
---
+++ go.mod
@@ -0,0 +1,48 @@
+module github.com/appliedres/cloudy/azure
+
+go 1.18
+
+replace github.com/appliedres/cloudy => ../cloudy
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.0.0
+	github.com/appliedres/cloudy v0.0.0-00010101000000-000000000000
+	github.com/deckarep/golang-set v1.8.0
+	github.com/microsoft/kiota-abstractions-go v0.7.0
+	github.com/microsoft/kiota-authentication-azure-go v0.3.0
+	github.com/microsoftgraph/msgraph-sdk-go v0.23.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.4.0 // indirect
+	github.com/Jeffail/gabs/v2 v2.6.1 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/cjlapao/common-go v0.0.19 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-openapi/errors v0.19.8 // indirect
+	github.com/go-openapi/strfmt v0.21.2 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
+	github.com/microsoft/kiota-http-go v0.3.0 // indirect
+	github.com/microsoft/kiota-serialization-json-go v0.3.1 // indirect
+	github.com/microsoft/kiota-serialization-text-go v0.2.0 // indirect
+	github.com/microsoftgraph/msgraph-sdk-go-core v0.24.0 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
+	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+	go.mongodb.org/mongo-driver v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220511200225-c6db032c6c88 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
